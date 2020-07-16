3f51c3245e4ceecd42271269a080c38c2a1695de
MohBastawi <itmbastawi@hotmail.com>
2020-07-16 08:30:24 +0200

    order

--- orders/views/order.py
+++ orders/views/order.py
@@ -2,20 +2,24 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from ..forms import profile_form,item_form
 from django.contrib import messages
-
+from ..models import service_table
 
 @login_required
 def ask_service(request):
+    items = service_table.objects.all()
     user =request.user
     if request.method == 'POST':
         form = item_form(request.POST)
         if form.is_valid():
-            form.save()
+            item = form.save(commit=False)
+            item.created_user = user
+            item.save()
             messages.success(request, 'Server added !')
         form.clean()
     else :
         form = item_form()
-    return render(request,'order/ask_service.html',{'form':form})
+    return render(request,'order/ask_service.html',{'form':form,
+                                                    'items':items})
 
 
 @login_required
